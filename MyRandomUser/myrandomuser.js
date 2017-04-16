218dd3273ccceeaec9dd376edce6bfd4e193ed5d
Hans van der Linden <haj.vanderlinden@avans.nl>
2017-04-16 19:17:45 +0200

    JSON output added

--- MyRandomUser/myrandomuser.js
+++ MyRandomUser/myrandomuser.js
@@ -22,6 +22,27 @@ app.put('/', function(request, response) {
     response.send('Hello Avans, PUT request received!');
 })
 
+app.get('/json', function(request, response) {
+    response.json({
+        'some_name': 'Value',
+        'an_array_of_objects': [
+            {
+                'another_name': 'Another value',
+                'a_further_name': "A further value"
+            },
+            {'yet_another_name': 'Yet another value'}
+        ],
+        'some_boolean': true,
+        'some_integer': 42,
+        'array_of_ints': [
+            2, 3, 5, 7, 11, 13
+        ],
+        'array_of_strings': [
+            "twee", "drie", "vijf", "zeven"
+        ]
+    })
+})
+
 app.all('*', function(request, response) {
     response.status(404);
     response.send('404 - Not found');
